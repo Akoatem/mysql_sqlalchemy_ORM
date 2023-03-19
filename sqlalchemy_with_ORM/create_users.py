ddab13933ee58493a9602c833bb2d5336dac0b91
Ako Atem <71437843+Akoatem@users.noreply.github.com>
2023-03-19 22:15:41 +0200

    updated

--- sqlalchemy_with_ORM/create_users.py
+++ sqlalchemy_with_ORM/create_users.py
@@ -2,37 +2,45 @@ from main import User, Session, engine, User
 
 # create a dictionary list of users
 
-users = [
+users=[
     {
-        "username": "Besson",
-        "email": "besson@gmail.com"
+        "username":"jerry",
+        "email":"jerry@company.com"
+    },
+     {
+        "username":"jordan",
+        "email":"jordan@company.com"
     }, {
-        "username": "Tesson",
-        "email": "tesson23@gmail.com"
+        "username":"jackson",
+        "email":"jackson@company.com"
     }, {
-        "username": "Ben56",
-        "email": "ben53@gmail.com"
+        "username":"jarden",
+        "email":"j@company.com"
     }, {
-        "username": "John23",
-        "email": "john35@gmail.com"
+        "username":"john",
+        "email":"john@company.com"
     }, {
-        "username": "suh364",
-        "email": "suh884@gmail.com"
+        "username":"jack",
+        "email":"jack@company.com"
     },
 ]
-# to create local session to connect database
-# Call the engine class using bind
-local_session = Session(bind=engine)
-# Create user to be added into the database
-# new_user = User(username="Akoatem", email="dsjhdjs@gmail.com")
 
-# to add new user to the database
+
+
+local_session=Session(bind=engine)
+
+# new_user=User(username="jona",email="jona@company.com")
+
 # local_session.add(new_user)
 
 # local_session.commit()
 
-for u in users:
-    new_user = User(username=u["username"], email=u["email"])
 
+for u in users:
+    new_user=User(username=u["username"],email=u["email"])
+    
     local_session.add(new_user)
+
     local_session.commit()
+
+    print(f"Added {u['username']}")
